a979d690b8ffc2484999637afcdb9654cbf295fa
Abhi Hiremagalur and Jonathan Barnes <abhi@pivotallabs.com>
2014-04-15 16:22:58 -0700

    Cleans up .librarian directory left by chefSpec
    This was confusion soloist during the integration test run
    
    [#69554524]

--- spec/unit/spec_helper.rb
+++ spec/unit/spec_helper.rb
@@ -7,4 +7,5 @@ RSpec.configure do |config|
   config.version = '10.8.2' # FIXME: system ruby and fauxhai don't play nice
                             # since there is no 10.9.2.json file yet. We
                             # should submit a PR to fauxhai to fix this
+  config.after(:suite)  { FileUtils.rm_r('.librarian') }
 end
