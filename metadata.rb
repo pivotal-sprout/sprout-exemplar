58cec9c7ca9b3607479dc8b49d6c2652369f6925
aram price <aprice@pivotal.io>
2016-03-25 17:44:04 -0700

    Fix foodcritic and rubocop errors

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'mac_os_x'
 depends          'sprout-base'
+
+issues_url 'https://github.com/pivotal-sprout/sprout-exemplar/issues'
+source_url 'https://github.com/pivotal-sprout/sprout-exemplar'
