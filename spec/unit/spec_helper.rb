57860a927fbe0e2f679a635f47ad3fd0ed5d816d
aram price <aprice@pivotal.io>
2016-05-17 11:06:19 -0700

    Update fauxhai mocking to use osx v10.11

--- spec/unit/spec_helper.rb
+++ spec/unit/spec_helper.rb
@@ -4,8 +4,7 @@ require 'chefspec/librarian'
 
 RSpec.configure do |config|
   config.platform = 'mac_os_x'
-  config.version = '10.9.2' # FIXME: change to 10.9.3.json when available ...
-  # see https://github.com/customink/fauxhai/tree/master/lib/fauxhai/platforms/mac_os_x
+  config.version = '10.11.1' # via https://github.com/customink/fauxhai/tree/master/lib/fauxhai/platforms/mac_os_x
   config.before { stub_const('ENV', 'SUDO_USER' => 'fauxhai') }
   config.after(:suite) { FileUtils.rm_r('.librarian') }
 end
